78d0cac4f528634fb3b89c2a872a7f83abe0537c
DonaldRR <13822743137@163.com>
2019-04-06 20:37:38 -0500

    model

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import tensorflow
 import keras
 from keras.models import Model, Input
-from keras.layers import BatchNormalization, Conv2D, MaxPool2D, Flatten, Dense, Dropout, Lambda, Reshape
+from keras.layers import BatchNormalization, Conv2D, MaxPool2D, Flatten, Dense, Dropout, Lambda, Reshape, Cropping2D
 from keras.optimizers import SGD, Adam
 from keras.backend import resize_images
 from keras.applications import vgg19, Xception, ResNet50
@@ -12,7 +12,8 @@ def model():
     input = Input(shape=(160, 320, 3), )
     print(input.get_shape())
     # Crop and resize input
-    input_ = Lambda(lambda x: x[:, 60:, :, :])(input)
+    input_ = Cropping2D(((60, 0), (0, 0)))(input)
+    # input_ = Lambda(lambda x: x[:, 60:, :, :])(input)
     # input_ = Lambda(lambda x: resize_images(x, 0.5, 0.5, data_format="channels_last"))(input_)
     # input_ = Lambda(lambda x: resize_images(x, ))
 
