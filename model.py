592e191e9107ab9f8a25f743984af3939db93331
DonaldRR <13822743137@163.com>
2019-04-06 20:08:45 -0500

    model

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from keras.models import Model, Input
 from keras.layers import BatchNormalization, Conv2D, MaxPool2D, Flatten, Dense, Dropout, Lambda, Reshape
 from keras.optimizers import SGD, Adam
 from keras.backend import resize_images
+from keras.applications import vgg19, Xception, ResNet50
 
 # Model creator
 def model():
@@ -11,14 +12,19 @@ def model():
     input = Input(shape=(160, 320, 3), )
     print(input.get_shape())
     # Crop and resize input
-    #     input_ = Lambda(lambda x: x[:, 60:, :, :])(input)
-    # input_ = Lambda(lambda x: resize_images(x, 0.5, 0.5, data_format="channels_last"))(input)
+    input_ = Lambda(lambda x: x[:, 60:, :, :])(input)
+    # input_ = Lambda(lambda x: resize_images(x, 0.5, 0.5, data_format="channels_last"))(input_)
+    # input_ = Lambda(lambda x: resize_images(x, ))
 
     # Convolutions
-    conv = BatchNormalization()(input)
+    # base = vgg19.VGG19(include_top=False)()
+
+    conv = BatchNormalization()(input_)
     conv = Conv2D(8, (3, 3), activation='relu')(conv)
+    # conv = Conv2D(16, (3, 3, ), activation='relu')(conv)
     conv = MaxPool2D((2, 2))(conv)
-    conv = Conv2D(16, (3, 3), activation='relu')(conv)
+    conv = Conv2D(32, (3, 3), activation='relu')(conv)
+    # conv = Conv2D(32, (3, 3), activation='relu')(conv)
     conv = MaxPool2D((2, 2))(conv)
 
     # Denses
