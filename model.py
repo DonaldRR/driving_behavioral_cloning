4a3baf24b1b13da2e3b4fc862fb22dc547eaad81
DonaldRR <13822743137@163.com>
2019-04-06 23:06:13 -0500

    model

--- model.py
+++ model.py
@@ -11,31 +11,31 @@ from keras.applications.resnet50 import ResNet50
 # Model creator
 def model():
     # Input
-    input = Input(shape=(224, 224, 3), )
+    input = Input(shape=(160, 320, 3), )
 
     # Crop and resize input
-    #     input_ = Cropping2D(((61, 0), (0, 0)))(input)
+    input_ = Cropping2D(((61, 0), (0, 0)))(input)
     #     input_ = Lambda(lambda x: resize_images(x, 0.5, 0.5, data_format="channels_last"))(input)
 
-    vgg19_base = VGG19(include_top=False)
-    for i in range(len(vgg19_base.layers)):
-        vgg19_base.layers[i].trainable = False
-    for i in range(15):
-        vgg19_base.layers.pop()
+    # vgg19_base = VGG19(include_top=False)
+    # for i in range(len(vgg19_base.layers)):
+    #     vgg19_base.layers[i].trainable = False
+    # for i in range(15):
+    #     vgg19_base.layers.pop()
 
         # Convolutions
-    #     conv = BatchNormalization()(input_)
-    #     conv = Conv2D(16, (3, 3), activation='relu')(conv)
-    #     conv = Conv2D(16, (3, 3), activation='relu')(conv)
-    #     conv = MaxPool2D((2, 2))(conv)
-    #     conv = Conv2D(32, (3, 3), activation='relu')(conv)
-    #     conv = Conv2D(32, (3, 3), activation='relu')(conv)
-    #     conv = MaxPool2D((2, 2))(conv)
+    conv = BatchNormalization()(input_)
+    conv = Conv2D(16, (3, 3), activation='relu')(conv)
+    conv = Conv2D(16, (3, 3), activation='relu')(conv)
+    conv = MaxPool2D((2, 2))(conv)
+    conv = Conv2D(32, (3, 3), activation='relu')(conv)
+    conv = Conv2D(32, (3, 3), activation='relu')(conv)
+    conv = MaxPool2D((2, 2))(conv)
 
-    vgg = vgg19_base(input)
+    # vgg = vgg19_base(input)
 
     # Denses
-    dense = Flatten()(vgg)
+    dense = Flatten()(conv)
     dense = Dense(32)(dense)
     dense = Dropout(0.5)(dense)
     dense = Dense(16)(dense)
